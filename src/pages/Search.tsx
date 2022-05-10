c99705fc18cbb3fdb106ce72a784a7ab228e948a
seananth <seansafstrom@gmail.com>
2022-05-10 10:37:03 +0200

    routing

---
+++ src/pages/Search.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Container from '@mui/material/Container';
+
+
+export default function Search() {
+    const API_KEY = "d0051e764e60b395b912da9a68a2327b"; //should be in a .env
+
+  return (
+    <Container>
+        hey
+    </Container>
+  )
+}
