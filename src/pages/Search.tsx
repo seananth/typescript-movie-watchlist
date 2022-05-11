7dcd8546e0b0d81c794ae8656934220c7ba1c8b7
seananth <seansafstrom@gmail.com>
2022-05-11 13:32:17 +0200

    useDebounce Hook

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -6,15 +6,20 @@ import Navbar from "../components/Navbar";
 import MovieListSection from "../components/MovieListSection";
 //interface
 import { movieProps } from "../interfaces/interfaces";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+//hooks
+import useDebounce from "../hooks/useDebounce";
 
 export default function Search() {
+  const API_KEY = "d0051e764e60b395b912da9a68a2327b"; //should be in a .env
   const [popular, setPopular] = useState<movieProps[]>([]);
   const [popularPage, setPopularPage] = useState(1);
+  const [searchResult, setSearchResult] = useState([]);
+  const [debouncedValue, value, setValue] = useDebounce<string>("", 200);
 
+  //get popular
   useEffect(() => {
     const apiPopular = async () => {
-      const API_KEY = "d0051e764e60b395b912da9a68a2327b"; //should be in a .env
       try {
         await axios
           .get(
@@ -30,10 +35,36 @@ export default function Search() {
     apiPopular();
   }, [popularPage]);
 
+  //search
+  useEffect(() => {
+    if (debouncedValue !== "") {
+      try {
+        axios
+          .get(
+            `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${debouncedValue}`
+          )
+          .then((res) => {
+            setSearchResult(res.data.results);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [debouncedValue]);
+
   return (
     <>
       <Navbar />
       <Container>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          onChange={(e) => setValue(e.target.value)}
+        />
+
+        <MovieListSection list={searchResult} title="" />
+
         <Button
           disabled={popularPage === 1}
           onClick={() => setPopularPage((prev) => prev - 1)}
