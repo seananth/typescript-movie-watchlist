2f94aed96a385df1352fe8b37e0d3e65498c7f1e
seananth <seansafstrom@gmail.com>
2022-05-16 09:41:10 +0200

    pagination buttons component

---
+++ src/components/PopularMoviesNav.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+interface PopularMoviesNav {
+  popularPage: number;
+  setPopularPage: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const PopularMoviesNav = ({
+  popularPage,
+  setPopularPage,
+}: PopularMoviesNav) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        margin: "2em 0",
+      }}
+    >
+      <Button
+        variant="contained"
+        disabled={popularPage === 1}
+        onClick={() => setPopularPage((prev) => prev - 1)}
+      >
+        Prev page
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => setPopularPage((prev) => prev + 1)}
+      >
+        Next page
+      </Button>
+    </Box>
+  );
+};
+
+export default PopularMoviesNav;
